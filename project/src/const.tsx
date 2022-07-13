a8379a3efeccdc8c5d3df8f80f04b849f8d95323
Natalya <m_d.r.lecter@protonmail.com>
2022-07-13 16:17:37 +0400

    routes

---
+++ project/src/const.tsx
@@ -0,0 +1,14 @@
+export enum AppRoute {
+  Main = '/',
+  SignIn = '/sign-in',
+  MyList = '/my-list',
+  Film = '/films/:id',
+  AddReview = '/films/:id/review',
+  Player = '/player/:id',
+}
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
